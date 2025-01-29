911e5c49cf2d1206b867123fc09f7ca864576833
aopalka <79727669+aopalka@users.noreply.github.com>
2025-01-28 22:05:04 -0500

    2025.2.20
    Updating laser can

---
+++ BobcatLib/src/main/java/BobcatLib/Hardware/Sensors/SpatialSensor/Components/LaserCAN.java
@@ -0,0 +1,96 @@
+package BobcatLib.Hardware.Sensors.SpatialSensor.Components;
+
+import BobcatLib.Hardware.Sensors.SpatialSensor.Utility.DistanceMode;
+import BobcatLib.Logging.Alert;
+import BobcatLib.Logging.Alert.AlertType;
+import au.grapplerobotics.ConfigurationFailedException;
+import au.grapplerobotics.LaserCan;
+import au.grapplerobotics.interfaces.LaserCanInterface.RangingMode;
+
+public class LaserCAN implements RangeSensor {
+  public int id;
+  public LaserCan tof;
+  public DistanceMode mode;
+  public double range;
+  public Alert sensorAlert;
+  public double sampleTime;
+  public boolean enable = false;
+
+  public LaserCAN(int id, DistanceMode mode, double sampleTime) {
+    this.id = id;
+    this.sampleTime = sampleTime;
+    this.mode = mode;
+    try {
+      tof = new LaserCan(id);
+      configRangeSensor();
+      enable = true;
+    } catch (Exception e) {
+      // TODO: handle exception
+      AlertType level = AlertType.INFO;
+      sensorAlert = new Alert("TOF", "TOF " + id + " hardware fault occured", level);
+      sensorAlert.set(true);
+    }
+  }
+
+  /**
+   * Gets the range in front of the sensor.
+   *
+   * @return range in mm
+   */
+  public double getRange() {
+    range = 0;
+    if (!enable) {
+      return 0;
+    }
+    LaserCan.Measurement measurement = tof.getMeasurement();
+    if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+      range = measurement.distance_mm;
+    }
+    return range;
+  }
+
+  public void configRangeSensor() {
+    // Optionally initialise the settings of the LaserCAN, if you haven't already
+    // done so in GrappleHook
+    try {
+      tof.setRangingMode(mode.asLaserCAN());
+      tof.setRegionOfInterest(new LaserCan.RegionOfInterest(0, 0, 16, 16));
+      tof.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_20MS);
+    } catch (ConfigurationFailedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void configRangeSensor(DistanceMode m) {
+    this.mode = m;
+    // Optionally initialise the settings of the LaserCAN, if you haven't already
+    // done so in GrappleHook
+    try {
+      tof.setRangingMode(m.asLaserCAN());
+      tof.setRegionOfInterest(new LaserCan.RegionOfInterest(0, 0, 16, 16));
+      tof.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_20MS);
+    } catch (ConfigurationFailedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public DistanceMode getMode() {
+    return mode;
+  }
+  /**
+   * Sets the distance mode of the sensor based on the provided distance.
+   *
+   * <p>RangingMode.Short: if distance less than 1250 mm RangingMode.Medium: if 1250 mm less than or
+   * equal too distance less than 2250 mm RangingMode.Long: if distance greater than or equal to
+   * 2250 mm
+   */
+  public DistanceMode getOptimalMode() {
+    double distance = getRange();
+    RangingMode mode =
+        (distance < 1000)
+            ? RangingMode.SHORT
+            : (distance < 1000) ? RangingMode.SHORT : RangingMode.LONG;
+    DistanceMode distanceMode = new DistanceMode();
+    return distanceMode.fromLaserCAN(mode);
+  }
+}
--- BobcatLib/src/main/java/BobcatLib/Hardware/Sensors/SpatialSensor/Utility/DistanceMode.java
+++ BobcatLib/src/main/java/BobcatLib/Hardware/Sensors/SpatialSensor/Utility/DistanceMode.java
@@ -11,6 +11,19 @@ public class DistanceMode {
 
   public modes currentMode;
 
+  public au.grapplerobotics.interfaces.LaserCanInterface.RangingMode asLaserCAN() {
+    if (currentMode == modes.SHORT) {
+      return au.grapplerobotics.interfaces.LaserCanInterface.RangingMode.SHORT;
+    }
+    if (currentMode == modes.MEDIUM) {
+      return au.grapplerobotics.interfaces.LaserCanInterface.RangingMode.SHORT;
+    }
+    if (currentMode == modes.LONG) {
+      return au.grapplerobotics.interfaces.LaserCanInterface.RangingMode.LONG;
+    }
+    return null;
+  }
+
   public RangingMode asPWF() {
     if (currentMode == modes.SHORT) {
       return RangingMode.Short;
@@ -36,6 +49,26 @@ public class DistanceMode {
       return nDistanceMode;
     }
     if (mode == RangingMode.Long) {
+      DistanceMode nDistanceMode = new DistanceMode();
+      nDistanceMode.currentMode = DistanceMode.modes.LONG;
+      return nDistanceMode;
+    }
+    return null;
+  }
+
+  public DistanceMode fromLaserCAN(
+      au.grapplerobotics.interfaces.LaserCanInterface.RangingMode mode) {
+    if (mode == au.grapplerobotics.interfaces.LaserCanInterface.RangingMode.SHORT) {
+      DistanceMode nDistanceMode = new DistanceMode();
+      nDistanceMode.currentMode = DistanceMode.modes.SHORT;
+      return nDistanceMode;
+    }
+    if (mode == au.grapplerobotics.interfaces.LaserCanInterface.RangingMode.SHORT) {
+      DistanceMode nDistanceMode = new DistanceMode();
+      nDistanceMode.currentMode = DistanceMode.modes.SHORT;
+      return nDistanceMode;
+    }
+    if (mode == au.grapplerobotics.interfaces.LaserCanInterface.RangingMode.LONG) {
       DistanceMode nDistanceMode = new DistanceMode();
       nDistanceMode.currentMode = DistanceMode.modes.LONG;
       return nDistanceMode;

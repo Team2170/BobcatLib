6f518b0e5a7c08c4c7527f00fb497ef215b4f498
aopalka <79727669+aopalka@users.noreply.github.com>
2025-02-04 09:55:50 -0500

    Updates and Example Project
    Example Motor Project
    Utilizes the BaseMotor type to do various functionality . RotateCW/CCW , SetPosition etc.

---
+++ Examples/ExampleMotorSubsystem/src/main/java/frc/robot/Commands/RotateMotorCCW.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Subsystems.FalconBasedSubsystem;
+import frc.robot.Subsystems.KrakenBasedSusbsystem;
+import frc.robot.Subsystems.NovaBasedSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class RotateMotorCCW extends Command {
+  private final Subsystem subsystem;
+  public double holdSpeed = 0.05;
+  public double operationalSpeed = 0.5;
+  /** Creates a new RotateMotorCW. */
+  public RotateMotorCCW(Subsystem sub) {
+    this.subsystem = sub;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.subsystem);
+  }
+
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if( subsystem instanceof NovaBasedSubsystem){
+      ((NovaBasedSubsystem)subsystem).runMotor(operationalSpeed);
+    }
+    else  if( subsystem instanceof KrakenBasedSusbsystem){
+      ((KrakenBasedSusbsystem)subsystem).runMotor(operationalSpeed);
+    }
+    else  if( subsystem instanceof FalconBasedSubsystem){
+
+      ((FalconBasedSubsystem)subsystem).runMotor(operationalSpeed);
+    }
+    else{
+        // Edge case I got nothing here.....
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    if( subsystem instanceof NovaBasedSubsystem){
+      ((NovaBasedSubsystem)subsystem).runMotor(holdSpeed);
+    }
+    else  if( subsystem instanceof KrakenBasedSusbsystem){
+
+      ((KrakenBasedSusbsystem)subsystem).runMotor(holdSpeed);
+    }
+    else  if( subsystem instanceof FalconBasedSubsystem){
+
+      ((FalconBasedSubsystem)subsystem).runMotor(holdSpeed);
+    }
+    else{
+        // Edge case I got nothing here.....
+    }
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ Examples/ExampleMotorSubsystem/src/main/java/frc/robot/Subsystems/NovaBasedSubsystem.java
@@ -0,0 +1,119 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import BobcatLib.Hardware.Motors.BaseMotor;
+import BobcatLib.Hardware.Motors.MotorConfigs;
+import BobcatLib.Hardware.Motors.ThriftyMotor;
+import BobcatLib.Hardware.Motors.SensorHelpers.NeutralModeWrapper.disabledMode;
+import BobcatLib.Hardware.Motors.Utility.SoftwareLimitWrapper;
+import BobcatLib.Hardware.Motors.Utility.SoftwareLimitWrapper.SoftwareLimitType;
+import BobcatLib.Utilities.CANDeviceDetails;
+import BobcatLib.Utilities.CANDeviceDetails.Manufacturer;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class NovaBasedSubsystem extends SubsystemBase {
+  private BaseMotor mMotor;
+
+  /** Creates a new NovaBasedSubsystem. */
+  public NovaBasedSubsystem() {
+    CANDeviceDetails canDevice = new CANDeviceDetails(1, Manufacturer.Thrifty);
+    MotorConfigs mc = new MotorConfigs();
+    mc.motorToGearRatio = 1;
+    mc.canDevice = canDevice;
+    mc.isInverted = false;
+    mc.kP = 0.05;
+    mc.kI = 0.00;
+    mc.kD = 0.00;
+    mc.mode = disabledMode.Brake;
+    // Set any Software Limits
+    SoftwareLimitWrapper limits = new SoftwareLimitWrapper(0, 20, SoftwareLimitType.BOTH);
+    mMotor = new BaseMotor(new ThriftyMotor(canDevice, "rio", mc), limits);
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  /**
+   * This is a method that makes the arm move at your desired speed
+   * Positive values make it spin forward and negative values spin it in reverse
+   * 
+   * @param speed motor speed from -1.0 to 1, with 0 stopping it
+   */
+  public void runMotor(double speed) {
+    mMotor.setSpeed(speed, 1, false);
+  }
+
+  /**
+   * This is a method that makes the subsystem motor go to a set position.
+   * 
+   * @param position motor position in rotations
+   */
+  public void runMotorToPosition(double position) {
+    mMotor.setAngle(position);
+  }
+
+
+
+  /**
+   * Implements a command composition style command to the subsystem that enables us to 
+   * move the motor to a set position. This will take the input you provide in rotations and set that as the desired rotation output.
+   * @return command 
+   */
+  public Command rotateMotorCW(){
+    Command moveMotorCW = new InstantCommand(() -> runMotor(0.5));
+    return moveMotorCW;
+  }  
+
+  /**
+   * Implements a command composition style command to the subsystem that enables us to 
+   * move the motor to a set position. This will do it at half a rotation output.
+   * @return command 
+   */
+  public Command rotateMotorCCW(){
+    Command moveHalfRotation = new InstantCommand(() -> runMotorToPosition(0.5));
+    return moveHalfRotation;
+  }  
+
+
+  /**
+   * Implements a command composition style command to the subsystem that enables us to 
+   * move the motor in a counter-clockwise direction to operate the subsystem. This will do it at 50% output.
+   * nothing fancy here and this is not PID based.
+   * @param angle in rotations you want the motor to turn too.
+   * @return command
+   */
+  public Command rotateSetPosition(double rotations){
+    Command moveMotorCW = new InstantCommand(() -> runMotor(rotations));
+    return moveMotorCW;
+  }  
+  /**
+   * Implements a command composition style command to the subsystem that enables us to 
+   * move the motor in a counter-clockwise direction to operate the subsystem. This will do it at 180 degrees or 0.5 output.
+   * nothing fancy here and this is not PID based. 
+   * @return command
+   */
+  public Command rotateSetPosition(){
+    Command moveMotorCW = new InstantCommand(() -> runMotor(0.5));
+    return moveMotorCW;
+  }  
+
+
+   /**
+   * Implements a command composition style command to the subsystem that enables us to 
+   * move the to enable the position to be held to operate the subsystem. 
+   * @return
+   */
+  public Command holdPosition(){
+    Rotation2d motorPosition = mMotor.getPosition();
+    Command moveMotorCW = new InstantCommand(() -> runMotorToPosition(motorPosition.getRotations()));
+    return moveMotorCW;
+  }
+
+}

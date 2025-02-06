3aee54001322146a5f355f5cc154c4fa778151d5
aopalka <79727669+aopalka@users.noreply.github.com>
2025-02-06 13:04:13 -0500

    Update to latest BobcatLib

--- Examples/SimpleSwerve/src/main/java/frc/robot/RobotContainer.java
+++ Examples/SimpleSwerve/src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import java.util.List;
+
 import BobcatLib.Hardware.Controllers.OI;
 import BobcatLib.Subsystems.Swerve.SimpleSwerve.Containers.SwerveBase;
 import BobcatLib.Subsystems.Swerve.SimpleSwerve.Swerve.Module.Utility.PIDConstants;
@@ -13,7 +14,6 @@ import BobcatLib.Subsystems.Swerve.Utility.LoadablePathPlannerAuto;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
